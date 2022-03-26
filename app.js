07524d25452d8bd8b56883f17a680adefe43f257
felipelopezortega <flopezortega@ucol.mx>
2022-03-26 11:15:53 -0600

    the debug function made with app.use is done and tested successfully

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var port = process.env.PORT || 3000; //setteamos el puerto para que escuche al s
 
 app.use("/assets",express.static(__dirname + "/public"));
 
+app.use("/", function(req, res, next){
+
+    console.log("Request Url;" + req.url);
+    next();
+});
+
 //primera ruta (está al nivel de la raíz/), Hello World!
 app.get("/", function(req, res){
 
@@ -13,7 +19,14 @@ app.get("/", function(req, res){
 
 app.listen(port) //levantar el server y ponerlo a la escucha
 
-//segunda ruta /api, regresa un objeto JSON
+
+//segunda ruta, recibe un parámetro
+app.get("/person/:id", function(req, res){
+
+    res.send("<html><head></head><body><h1>Person:" + req.params.id + "</h1></body></html>");
+});
+
+//tercera ruta /api, regresa un objeto JSON
 
 app.get("/api", function(req, res){
 
@@ -21,8 +34,3 @@ app.get("/api", function(req, res){
 
 });
 
-//tercera ruta, recibe un parámetro
-app.get("/person/:id", function(req, res){
-
-    res.send("<html><head></head><body><h1>Person:" + req.params.id + "</h1></body></html>");
-});
